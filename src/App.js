0e56f05c655f58d036a687dc893d605ff090267e
David Valdez <dmvdev.22@gmail.com>
2022-03-25 15:15:10 +0800

    remove hardcode item and get and display the real items from api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import { useState } from 'react';
+import axios from 'axios';
 import styled from 'styled-components';
-import { 
-  Header, 
-  AppNameComponent, 
-  AppIcon, 
-  SearchComponent, 
-  SearchIcon, 
-  SearchInput 
+
+import {
+  Header,
+  AppNameComponent,
+  AppIcon,
+  SearchComponent,
+  SearchIcon,
+  SearchInput
 } from './components/HeaderComponent';
 
 import {
@@ -15,6 +18,10 @@ import {
   IngredientsText,
   CompleteRecipe
 } from './components/RecipeComponent';
+
+
+const APP_ID = "b8481ba5";
+const APP_KEY = "16c7f9de9ce6dee28a50b50621ab4669";
 
 const Container = styled.div`
   display: flex;
@@ -30,53 +37,52 @@ const RecipeListContainer = styled.div`
   justify-content: space-evenly;
 `;
 
+const RecipeComponent = (props) => {
+  const { recipeObj } = props;
+  return (
+    <RecipeContainer>
+      <CoverImage src={recipeObj.image} />
+      <RecipeName>{recipeObj.label}</RecipeName>
+      <IngredientsText>Ingredients</IngredientsText>
+      <CompleteRecipe onClick={() => window.open(recipeObj.url)}>Complete Recipe</CompleteRecipe>
+    </RecipeContainer>
+  );
+};
+
 function App() {
+  const [timeoutId, updateTimoutId] = useState();
+  const [recipeList, updateRecipeList] = useState([]);
+
+  const fetchRecipe = async (searchString) => {
+    const response = await axios.get(
+      `https://api.edamam.com/api/recipes/v2?type=public&q=${searchString}&app_id=${APP_ID}&app_key=${APP_KEY}`
+    );
+    updateRecipeList(response.data.hits);
+  };
+
+  const onTextChange = (event) => {
+    clearTimeout(timeoutId)
+    const timeout = setTimeout(() => fetchRecipe(event.target.value), 500);
+    updateTimoutId(timeout);
+  };
+
+
   return (
     <Container>
       <Header>
         <AppNameComponent><AppIcon src='/recipe-icon.svg' />Find Food Recipe</AppNameComponent>
         <SearchComponent>
           <SearchIcon src="/search-icon.svg" />
-          <SearchInput placeholder='Search Food' />
+          <SearchInput placeholder='Search Food' onChange={onTextChange} />
         </SearchComponent>
       </Header>
       <RecipeListContainer>
-        <RecipeContainer>
-          <CoverImage src="/recipe-icon.svg" alt="" />
-          <RecipeName>Eggs</RecipeName>
-          <IngredientsText>Ingredients</IngredientsText>
-          <CompleteRecipe>Complete Recipe</CompleteRecipe>
-        </RecipeContainer>
-        <RecipeContainer>
-          <CoverImage src="/recipe-icon.svg" alt="" />
-          <RecipeName>Eggs</RecipeName>
-          <IngredientsText>Ingredients</IngredientsText>
-          <CompleteRecipe>Complete Recipe</CompleteRecipe>
-        </RecipeContainer>
-        <RecipeContainer>
-          <CoverImage src="/recipe-icon.svg" alt="" />
-          <RecipeName>Eggs</RecipeName>
-          <IngredientsText>Ingredients</IngredientsText>
-          <CompleteRecipe>Complete Recipe</CompleteRecipe>
-        </RecipeContainer>
-        <RecipeContainer>
-          <CoverImage src="/recipe-icon.svg" alt="" />
-          <RecipeName>Eggs</RecipeName>
-          <IngredientsText>Ingredients</IngredientsText>
-          <CompleteRecipe>Complete Recipe</CompleteRecipe>
-        </RecipeContainer>
-        <RecipeContainer>
-          <CoverImage src="/recipe-icon.svg" alt="" />
-          <RecipeName>Eggs</RecipeName>
-          <IngredientsText>Ingredients</IngredientsText>
-          <CompleteRecipe>Complete Recipe</CompleteRecipe>
-        </RecipeContainer>
-        <RecipeContainer>
-          <CoverImage src="/recipe-icon.svg" alt="" />
-          <RecipeName>Eggs</RecipeName>
-          <IngredientsText>Ingredients</IngredientsText>
-          <CompleteRecipe>Complete Recipe</CompleteRecipe>
-        </RecipeContainer>
+        {recipeList.length && recipeList.map((recipeObj) => (
+          <RecipeComponent recipeObj={recipeObj.recipe} />
+        ))}
+
+
+
       </RecipeListContainer>
     </Container>
   );
